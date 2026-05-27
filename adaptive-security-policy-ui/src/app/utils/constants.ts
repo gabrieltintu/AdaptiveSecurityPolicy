c0280bb312d25f0b814347b556224973fb3a5e2b
Gabriel Tintu <gabriel.tintu@yahoo.com>
2026-05-27 19:02:32 +0300

    fix cors errors and api url

--- adaptive-security-policy-ui/src/app/utils/constants.ts
+++ adaptive-security-policy-ui/src/app/utils/constants.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8090/api';
-export const WS_URL      = 'http://localhost:8090/ws';
+export const API_BASE_URL = 'http://192.168.64.4:8090/api';
+export const WS_URL      = 'http://192.168.64.4:8090/ws';
 
 export const BRUTE_FORCE_WARNING_THRESHOLD = 5;
 export const BRUTE_FORCE_BLOCK_THRESHOLD   = 10;
