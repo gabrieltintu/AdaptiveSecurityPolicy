1f6c903ce532873cc376bf5a89bc8e6051d154c6
Gabriel Tintu <gabriel.tintu@yahoo.com>
2026-06-09 01:42:19 +0300

    fix colors

--- adaptive-security-policy-ui/src/app/theme/asp-preset.ts
+++ adaptive-security-policy-ui/src/app/theme/asp-preset.ts
@@ -4,7 +4,7 @@ import Aura from '@primeuix/themes/aura';
 export const AspPreset = definePreset(Aura, {
   semantic: {
     primary: {
-      50:  '#faf5ff',
+      50: '#faf5ff',
       100: '#f3e8ff',
       200: '#e9d5ff',
       300: '#d8b4fe',
@@ -19,18 +19,18 @@ export const AspPreset = definePreset(Aura, {
     colorScheme: {
       dark: {
         surface: {
-          0:   '#ffffff',
-          50:  '#f6f2fb',
-          100: '#e9e0f4',
-          200: '#cdbce4',
-          300: '#a888c9',
-          400: '#7e57a8',
-          500: '#5b3a85',
-          600: '#472d6b',
-          700: '#34204f',
-          800: '#22153a',
-          900: '#160a26',
-          950: '#0d0518'
+          0: '#0d0518',
+          50: '#160a26',
+          100: '#22153a',
+          200: '#34204f',
+          300: '#472d6b',
+          400: '#5b3a85',
+          500: '#7e57a8',
+          600: '#a888c9',
+          700: '#cdbce4',
+          800: '#e9e0f4',
+          900: '#f6f2fb',
+          950: '#ffffff'
         },
         primary: {
           color: '{primary.400}',
